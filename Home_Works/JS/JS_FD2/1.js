7ba7f8ac34a0172e5828120c4fef7584f439fb7e
AlexLeoChumak <chumak1106@gmail.com>
2023-02-13 17:35:47 +0300

    FD2 correct lesson1-3

--- Home_Works/JS/JS_FD2/1.js
+++ Home_Works/JS/JS_FD2/1.js
@@ -1,117 +1,99 @@
 'use strict';
-/*
 
-const length = prompt('Введите длину прямоугольника');
-const width = prompt('Введите ширину прямоугольника');
 
-if ( isNaN(length) && isNaN(width) ) {
-  console.log(`Некорректный ввод длины и ширины (введите числа). 
-               Для повтора ввода нажмите F5`);
-} else if ( length === '' && width === '' ) {
-  console.log(`Некорректный ввод длины и ширины (пустые строки). 
-               Для повтора ввода нажмите F5`);
-} else if ( isNaN(length) ) {
-  console.log(`Некорректный ввод длины (введите число).
-               Для повтора ввода нажмите F5`);
-} else if ( isNaN(width) ) {
-  console.log(`Некорректный ввод ширины (введите число). 
-               Для повтора ввода нажмите F5`);  
-} else if ( length === '' ) {
-  console.log(`Некорректный ввод длины (пустая строка).
-                Для повтора ввода нажмите F5`);
-} else if ( width === '' ) {
-  console.log(`Некорректный ввод ширины (пустая строка). 
-                Для повтора ввода нажмите F5`);  
-} else if ( length === null || width === null ) {
-  console.log(`Вы отменили ввод. 
-            Для повтора ввода нажмите F5`);
-} else {
-  calcArea(length, width);
-  calcPerimeter(length, width);
-}
+// const length = prompt('Введите длину прямоугольника');
+// const width = prompt('Введите ширину прямоугольника');
 
-function calcArea(length, width) {
-  const areaRectangle = length * width;
-  console.log(`Площадь геометрической фигуры составляет: 
-               ${areaRectangle} единиц.`);
-}
+// if ( isNaN(length) && isNaN(width) ) {
+//   console.log(`Некорректный ввод длины или (и) ширины (введите числа).`);
+// } else if ( length === null || width === null ) {
+//   console.log(`Вы отменили ввод.`);
+// } else if ( length == false || width == false || length < 1 || width < 1 ) {
+//   console.log(`Некорректный ввод длины или (и) ширины.`);
+// } else {
+//   calcArea(length, width);
+//   calcPerimeter(length, width);
+//   if ( length === width) {
+//     console.log('Это квадрат');
+//   }
+// }
 
-function calcPerimeter(length, width) {
-  const perimeterRectangle = (Number(length) + Number(width)) * 2;
-  console.log(`Периметр геометрической фигуры составляет: 
-               ${perimeterRectangle} единиц.`);
-}
+// function calcArea(length, width) {
+//   const areaRectangle = length * width;
+//   console.log(`Площадь геометрической фигуры составляет: 
+//                ${areaRectangle} единиц.`);
+// }
 
-if ( length === width 
-  && length !== null 
-  && length !== '' ) {
-  console.log('Это квадрат');
-} */
-  
-/*  */
-
-/* const month = Number(prompt('Введите номер месяца (число)'));
-
-if ( month === 12 || month === 1 || month === 2 ) {
-  console.log('Пора года - зима');
-} else if ( month === 3 || month === 4 || month === 5 ) {
-  console.log('Пора года - весна');
-} else if ( month === 6 || month === 7 || month === 8 ) {
-  console.log('Пора года - лето');
-} else if ( month === 9 || month === 10 || month === 11 ) {
-  console.log('Пора года - осень');
-} else if ( month >= 13 ) {
-  console.log('Такого порядкового числа месяца в году не существует');
-} else {
-  console.log('Неверное значение');
-} */
+// function calcPerimeter(length, width) {
+//   const perimeterRectangle = (Number(length) + Number(width)) * 2;
+//   console.log(`Периметр геометрической фигуры составляет: 
+//                ${perimeterRectangle} единиц.`);
+// }
 
 /*  */
 
-/* const month = Number(prompt('Введите номер месяца (число)'));
+// const month = Number(prompt('Введите номер месяца (число)'));
 
-const timeYear = ( month === 12 || month === 1 || month === 2 ) ? 'Пора года - зима' :
-  ( month === 3 || month === 4 || month === 5 ) ? 'Пора года - весна' :
-  ( month === 6 || month === 7 || month === 8 ) ? 'Пора года - лето' :
-  ( month === 9 || month === 10 || month === 11 ) ? 'Пора года - осень' :
-  ( month >= 13 ) ? 'Такого порядкового числа месяца в году не существует' :
-  'Неверное значение';
-
-console.log(timeYear); */
+// if ( month === 12 || month === 1 || month === 2 ) {
+//   console.log('Пора года - зима');
+// } else if ( month === 3 || month === 4 || month === 5 ) {
+//   console.log('Пора года - весна');
+// } else if ( month === 6 || month === 7 || month === 8 ) {
+//   console.log('Пора года - лето');
+// } else if ( month === 9 || month === 10 || month === 11 ) {
+//   console.log('Пора года - осень');
+// } else if ( month >= 13 ) {
+//   console.log('Такого порядкового числа месяца в году не существует');
+// } else {
+//   console.log('Неверное значение');
+// }
 
 /*  */
 
-/* const month = Number(prompt('Введите номер месяца (число)'));
+// const month = Number(prompt('Введите номер месяца (число)'));
 
-switch (month) {
-  case 12: 
-  case 1: 
-  case 2:
-    console.log('Пора года - зима');
-    break;
+// const timeYear = ( month === 12 || month === 1 || month === 2 ) ? 'Пора года - зима' :
+//   ( month === 3 || month === 4 || month === 5 ) ? 'Пора года - весна' :
+//   ( month === 6 || month === 7 || month === 8 ) ? 'Пора года - лето' :
+//   ( month === 9 || month === 10 || month === 11 ) ? 'Пора года - осень' :
+//   ( month >= 13 ) ? 'Такого порядкового числа месяца в году не существует' :
+//   'Неверное значение';
 
-  case 3: 
-  case 4: 
-  case 5:
-    console.log('Пора года - весна');
-    break;
+// console.log(timeYear);
 
-  case 6: 
-  case 7: 
-  case 8:
-    console.log('Пора года - лето');
-    break;
+/*  */
 
-  case 9: 
-  case 10: 
-  case 11:
-    console.log('Пора года - осень');
-    break;
+// const month = Number(prompt('Введите номер месяца (число)'));
 
- default:
-    console.log('Неверное значение');
-}
- */
+// switch (month) {
+//   case 12: 
+//   case 1: 
+//   case 2:
+//     console.log('Пора года - зима');
+//     break;
+
+//   case 3: 
+//   case 4: 
+//   case 5:
+//     console.log('Пора года - весна');
+//     break;
+
+//   case 6: 
+//   case 7: 
+//   case 8:
+//     console.log('Пора года - лето');
+//     break;
+
+//   case 9: 
+//   case 10: 
+//   case 11:
+//     console.log('Пора года - осень');
+//     break;
+
+//  default:
+//     console.log('Неверное значение');
+// }
+
 
 /*  */
 
@@ -131,7 +113,7 @@ function showErrorZero(num) {
 
 showErrorZero(num);
 
-function checkEscEmptyStr(numStr) { //перепробовал много способов - функция работает неверно
+function checkEscEmptyStr(numStr) { 
   if (numStr === '0' || typeof(numStr) === 'string') {
     console.log('Это целое чётное не положительное и не отрицательное число');
   } else if (numStr === '' || typeof(numStr) === 'string') {
@@ -169,29 +151,37 @@ function checkNigativPositiv(num) {
 
 /*  */
 
-/* let symbols = '|_';
-let counter = 1;
 
-while (counter <= 9) {
-  console.log(symbols);
-  symbols += '|_';
-  counter++;
-} */
+// let symbols = '|_';
+// let counter = 1;
+
+// while (counter <= 9) {
+//   console.log(symbols);
+//   symbols += '|_';
+//   counter++;
+// }
+
 
 /*  */
 
-/* let whiteBlackSquare = '\u2B1C \u2B1B \u2B1C \u2B1B \u2B1C \u2B1B \u2B1C \u2B1B \u000A';
-let blackWhiteSquare = '\u2B1B \u2B1C \u2B1B \u2B1C \u2B1B \u2B1C \u2B1B \u2B1C \u000A';
-let result = '';
-let i;
-let j;
 
-for (i = 1; i < 5; i++) {
-  result += whiteBlackSquare;
-  
-  for (j = 1; j < 2 ; j++) {
-    result += blackWhiteSquare;
-  } 
-}
+// const whiteSquare = '\u2B1C';
+// const blackSquare = '\u2B1B';
+// let oddEvenValue = 0;
+// let result = '';
 
-console.log(result); */
+// for (let i = 0; i < 8; i++) {
+//   for (let j = 0; j < 8; j++) {
+
+//     if ((j + oddEvenValue) % 2 == 0) {
+//       result += whiteSquare;
+//     } else {
+//       result += blackSquare;
+//     }
+//   }
+
+//   console.log(result);
+
+//   result = '';
+//   oddEvenValue += 1;
+// }
