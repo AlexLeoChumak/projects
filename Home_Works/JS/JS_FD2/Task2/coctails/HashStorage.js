3bacbd56649e1527d64c5587bbc47180c897b888
AlexLeoChumak <chumak1106@gmail.com>
2023-02-28 15:30:20 +0300

    Practical task #12 (drinks)

--- Home_Works/JS/JS_FD2/Task2/coctails/HashStorage.js
+++ Home_Works/JS/JS_FD2/Task2/coctails/HashStorage.js
@@ -1,57 +1,32 @@
 'use strict';
 
 class HashStorage {
-    constructor() {
-      this.store_ = {}; // хранилище для значений
-    }
-  
-    add(key, value) {
-      this.store_[key] = value; // сохраняем значение в store
-    }
-  
-    getValue(key) {
-      return this.store_[key]; // тут все тоже просто и нативно
-    }
-  
-    deleteValue(key) {
-      if(!(key in this.store_)) {
-        return false
-      }
-  
-      return delete this.store_[key];
-    }
-  
-    getKeys() {
-      return Object.keys(this.store_);
-    }
-  }
-  
-const coctailsStorage = new HashStorage();
+	constructor() {
+		this.hashStorage = {};
+	}
 
+	addValue(key, value) {
+		this.hashStorage[key] = value;
+	}
 
-// class HashStorage {
-//     constructor(coctail, alcogol, ingredients = [], recipe) {
-//         this.coctail = coctail;
-//         this.alcogol = alcogol;
-//         this.ingredients = ingredients;
-//         this.recipe = recipe;
-//     }
+	getValue(key) {
+		if (key in this.hashStorage) {
+			return this.hashStorage[key];
+		} else {
+			return undefined;
+		}
+	}
 
-//     addValue(key, value) {
-//         this.key = value;
-//     }
-//     getValue(key) {
-//         return (key) ? key.value : undefined;
-//     }
-//     deleteValue(key) {
-//         delete this.key;
-//     }
-//     getKeys() {
-//         return Object.keys(this);
-//     }
-// };
+	deleteValue(key) {
+		if (key in this.hashStorage) {
+			delete this.hashStorage[key];
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-
-
-
-
+	getKeys() {
+		return Object.keys(this.hashStorage);
+	}
+}
